0fc6e8c55bfc3e71fa0d70797666afb32c654348
Douglas Victor Cedrum <douglsa.viced@gmail.com>
2020-06-19 13:45:12 +0200

    Fixed styling and added totals in cart app

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 from decimal import Decimal
 
 def cart_contents(request):
@@ -6,9 +8,20 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id' : item_id,
+            'quantity' : quantity,
+            'product' : product,
+        })
 
     if total < settings.FREE_SHIPPING_TRESHOLD:
-        shipping = Decimal(settings.STANDARD_SHIPPING_PERCENTAGE)
+        shipping = total * Decimal(settings.STANDARD_SHIPPING_PERCENTAGE / 100)
         free_shipping_remaining = settings.FREE_SHIPPING_TRESHOLD - total
     else:
         shipping = 0
