b8a06e2da6634d4b4077812c2ad9c648a699a712
Douglas Victor Cedrum <douglsa.viced@gmail.com>
2020-06-23 15:42:18 +0200

    Added toast when adding item to cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
  
 
 # Create your views here.
@@ -12,11 +15,14 @@ def add_to_cart(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
+    product = Product.objects.get(pk=item_id)
+
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
