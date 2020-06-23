79aea3403ca033c88cfc3906c6ede9eb2308396e
Douglas Victor Cedrum <douglsa.viced@gmail.com>
2020-06-23 16:05:01 +0200

    added toast messages when removing items

--- cart/views.py
+++ cart/views.py
@@ -1,55 +1,54 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
- 
+
 
 # Create your views here.
 def view_cart(request):
-    
     """This view returns the cart page"""
     return render(request, 'cart/cart.html')
+
 
 def add_to_cart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    product = get_object_or_404(Product, pk=item_id)
     cart = request.session.get('cart', {})
-    product = Product.objects.get(pk=item_id)
-
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'{quantity} {product.name} added to cart')
+
     else:
         cart[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your cart')
+        messages.success(request, f'{quantity} {product.name} added to cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
 
 def adjust_cart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
-    if quantity > 0:
-        cart[item_id] = quantity
-    else:
-        cart.pop[item_id]
+    cart[item_id] = quantity
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
 
+
 def remove_from_cart(request, item_id):
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         cart = request.session.get('cart', {})
         cart.pop(item_id)
-
+        messages.success(request, f'{product.name} removed from cart')
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
         return HttpResponse(status=500)
-
-
