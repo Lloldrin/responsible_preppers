e3b81941e7db15d9179f333feb91e754598cd3ea
Douglas Victor Cedrum <douglsa.viced@gmail.com>
2020-07-09 18:30:42 +0200

    improved webhook functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrdersForm
 from .models import Order, OrderLineItem
-from cart.contexts import cart_contents
 from products.models import Product
+from cart.contexts import cart_contents
 
 import stripe
+import json
 
-# Create your views here.
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(
+            request, 'Due to technical issues your payment can not be handled right now. Please try again later')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -68,7 +85,7 @@ def checkout(request):
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
-                
+
     else:
         cart = request.session.get('cart', {})
         if not cart:
