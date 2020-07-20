6842b7bf7761e537432756faf15cf85ee15e8cc4
Douglas Victor Cedrum <douglsa.viced@gmail.com>
2020-07-20 13:54:06 +0200

    added functionality to delete products

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('add_product/', views.add_product, name='add_product'),
     path('edit_product/<product_id>/', views.edit_product, name='edit_product'),
+    path('delete_product/<product_id>/', views.delete_product, name='delete_product'),
     path('<product_id>/', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -78,9 +78,9 @@ def add_product(request):
         # Including request.FILES makes sure that we get the images as well as the form data
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product added successfully!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Please make sure the form is correct!')
     else:
@@ -114,3 +114,10 @@ def edit_product(request, product_id):
     }
     
     return render(request, template, context)
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product successfully deleted!')
+    
+    return redirect(reverse('products'))   
