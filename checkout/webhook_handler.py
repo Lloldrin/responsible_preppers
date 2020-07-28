fd57de7482152ac5f236f6701bb9648ee699b73d
Douglas Victor Cedrum <douglsa.viced@gmail.com>
2020-07-28 15:52:56 +0200

    added confirmation emails on order completion

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,29 +1,31 @@
 from django.http import HttpResponse
 
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
+
 
 import json
 import time
 
 class StripeWH_Handler:
-    """Handle Stripe webhooks"""
+    # Handles stripe webhooks
 
     def __init__(self, request):
         self.request = request
 
     def handle_event(self, event):
-        """
-        Handle a generic/unknown/unexpected webhook event
-        """
+        # Handles webhooks that are not handled with a generic reply
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
 
     def handle_payment_intent_succeeded(self, event):
-        """
-        Handle the payment_intent.succeeded webhook from Stripe
-        """
+        # Handles successful payment intents from Stripe
         intent = event.data.object
         pid = intent.id
         cart = intent.metadata.cart
@@ -110,9 +112,23 @@ class StripeWH_Handler:
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
-        """
-        Handle the payment_intent.payment_failed webhook from Stripe
-        """
+        #Handles failed payment intent from Stripe
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
+
+def _send_confirmation_email(self, order):
+    # Sends confirmation email when order is processed
+    cust_email = order.email
+    subject = render_to_string(
+        'checkout/confirmation_emails/confirmation_email_subject.txt',
+        {'order': order})
+    body = render_to_string(
+        'checkout/confirmation_emails/confirmation_email_body.txt',
+        {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+    send_mail(
+        subject,
+        body,
+        settings.DEFAULT_FROM_EMAIL,
+        [cust_email]
+    )
