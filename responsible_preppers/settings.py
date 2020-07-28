d3fc5de9abcd5992621f674e9604e7f735516a45
Douglas Victor Cedrum <douglsa.viced@gmail.com>
2020-07-28 12:58:32 +0200

    changed 'localhost' to '127.0.0.1' in allowed host

--- responsible_preppers/settings.py
+++ responsible_preppers/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['responsible-preppers.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['responsible-preppers.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
